119ea8dfbde7316c633a57e797259e94b00375ab
Gris Ge <fge@redhat.com>
2025-08-07 19:15:46 +0800

    nmcli: Use clap
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -4,15 +4,55 @@ use nm::{NmClient, NmError};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), NmError> {
-    enable_logging();
+    let mut cli_cmd = clap::Command::new("nipc")
+        .about("NetworkManager CLI")
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .arg(
+            clap::Arg::new("quiet")
+                .short('q')
+                .action(clap::ArgAction::SetTrue)
+                .help("Disable logging")
+                .global(true),
+        )
+        .arg(
+            clap::Arg::new("verbose")
+                .short('v')
+                .action(clap::ArgAction::Count)
+                .help("Increase verbose level")
+                .global(true),
+        )
+        .subcommand(
+            clap::Command::new("ping").about("Check daemon connection"),
+        );
 
-    let mut cli = NmClient::new().await?;
-    println!("HAHA {:?}", cli.ping().await?);
+    let matches = cli_cmd.get_matches_mut();
+
+    let (log_groups, log_level) = match matches.get_count("verbose") {
+        0 => (vec!["nm", "nmstate"], log::LevelFilter::Info),
+        1 => (vec!["nm", "nmstate"], log::LevelFilter::Debug),
+        2 => (vec!["nm", "nmstate"], log::LevelFilter::Trace),
+        _ => (vec![""], log::LevelFilter::Trace),
+    };
+
+    if !matches.get_flag("quiet") {
+        let mut log_builder = env_logger::Builder::new();
+        if log_groups.is_empty() {
+            log_builder.filter(None, log_level);
+        } else {
+            for log_group in log_groups {
+                log_builder.filter(Some(log_group), log_level);
+            }
+        }
+        log_builder.init();
+    }
+
+    log::info!("nmcli version: {}", clap::crate_version!());
+
+    if let Some(_) = matches.subcommand_matches("ping") {
+        let mut cli = NmClient::new().await?;
+        println!("{}", cli.ping().await?);
+    }
+
     Ok(())
-}
-
-fn enable_logging() {
-    let mut log_builder = env_logger::Builder::new();
-    log_builder.filter(None, log::LevelFilter::Debug);
-    log_builder.init();
 }
