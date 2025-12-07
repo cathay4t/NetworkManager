b89f8c2e5d4d5eeb7620323f4f0219b58be3c6bf
Gris Ge <fge@redhat.com>
2025-12-07 08:12:30 +0800

    CI: Enable integration test
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/testlib/env.py
+++ tests/testlib/env.py
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
+import pathlib
 
 def is_fedora():
     return os.path.exists("/etc/fedora-release")
+
+def nmc_path():
+    project_dir = pathlib.Path(__file__).parent.parent.parent.resolve()
+    return f"{project_dir}/target/debug/nmc"
